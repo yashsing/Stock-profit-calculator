d210de038f5dfcd570ff79d273e8524dca5329cc
Yash Singhal <yashagarwal559@gmail.com>
2021-09-30 19:39:45 +0530

    initial

---
+++ app.js
@@ -0,0 +1,39 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var curr = Number(currentPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+
+    showOutput(
+      `Total loss is ${loss} and Total percent is ${lossPercentage}%`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+
+    showOutput(
+      `Total profit is ${profit} and Total percent is ${profitPercentage}%`
+    );
+  } else {
+    showOutput(`There is no change in current price, so NO LOSS & NO GAIN`);
+  }
+}
+
+function showOutput(message) {
+  outputBox.innerHTML = message;
+}
